0fd52e8f352bea256449b7e7ae5cbd2280ad073c
soddi <cvb7412@naver.com>
2020-04-02 17:01:36 +0900

    마스터 추가

--- Test.java
+++ Test.java
@@ -2,6 +2,7 @@ public class Test{
 	public static void main(string[] args{
 		System.out.println("hello");
 		System.out.println("hello World");
-		System.out.pribtln("hotfix add");
+		System.out.println("hotfix add");
+		System.out.println("master add");
 	}
 }
